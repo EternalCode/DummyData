0bbec75adf30bebd3eee279fd9ee42017cef76e0
EternalCode <dilshan.gunasekera@mail.utoronto.ca>
2020-01-05 23:55:43 -0500

    Added auto fill for arrays and short text

--- smarterdata.js
+++ smarterdata.js
@@ -4,9 +4,9 @@ function GetRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
-function RunRadio(mode, n = undefined) {
+function RunRadio() {
     let answers = document.getElementsByClassName("answer-item");
-    let checkedIndex = GetRandomInt(0, answers.length - 1);
+    let checkedIndex = GetRandomInt(0, answers.length);
     let radioId = answers[checkedIndex].id.replace("javatbd", "answer");
     let button = document.getElementById(radioId);
     button.checked = true;
@@ -17,8 +17,41 @@ function RunRadio(mode, n = undefined) {
     $('#movenextbtn, #movesubmitbtn').trigger('click');
 }
 
+function RunArray() {
+    let answers = document.getElementsByClassName("radio-list");
+    for (let row = 0; row < answers.length; row++) {
+        if (answers[row].style.cssText == "display: none;")
+            continue;
+        let radios = answers[row].getElementsByClassName("answer-item");
+        let checkedIndex = GetRandomInt(0, radios.length);
+        let radioId = answers[row].id.replace("javatbd", "answer");
+        let val = radios[checkedIndex].getElementsByClassName("radio").item(0).value
+        checkedIndex += 1;
+        radioId += "-" + val.toString();
+        console.log(radioId);
+        let button = document.getElementById(radioId);
+        button.checked = true;
+        button.onclick();
+    }
+}
+
+function ShortText() {
+    let answers = document.getElementsByClassName("answer-item");
+    answers[0].getElementsByClassName("form-control")[0].value = "Dummy data"
+}
+
 $(document).ready(function()  {
     let ddstatus = Cookies.get('runDD');
-    if (ddstatus == "1")
-        RunRadio();
+    if (ddstatus == "1") {
+        // run the correct DD
+        if (len(document.getElementsByClassName("list-radio")) > 0) {
+            RunRadio();
+        } else if(len(document.getElementsByClassName("array-flexible-row")) > 0) {
+            RunArray();
+        } else if(len(document.getElementsByClassName("text-short")) > 0) {
+            RunArray();
+        } else if(len(document.getElementsByClassName("boilerplate")) > 0) {
+            $('#movenextbtn, #movesubmitbtn').trigger('click');
+        }
+    }
 });
